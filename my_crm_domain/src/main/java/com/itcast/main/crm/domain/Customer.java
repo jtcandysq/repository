268c1dd1cbd484b0f0495e8224883c0c14f7e71d
jtcandysq <228852874@qq.com>
2017-09-24 01:32:31 +0800

    第一次提交

---
+++ my_crm_domain/src/main/java/com/itcast/main/crm/domain/Customer.java
@@ -0,0 +1,177 @@
+package com.itcast.main.crm.domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @description:客户信息�?
+ * 
+ */
+@Entity
+@Table(name = "T_CUSTOMER")
+@XmlRootElement(name = "customer")
+public class Customer {
+	@Id
+	@GeneratedValue()
+	@Column(name = "C_ID")
+	private Integer id; // 主键id
+	@Column(name = "C_USERNAME")
+	private String username; // 用户�?
+	@Column(name = "C_PASSWORD")
+	private String password; // 密码
+	@Column(name = "C_TYPE")
+	private Integer type; // 类型 设置1 绑定邮箱
+	@Column(name = "C_BRITHDAY")
+	@Temporal(TemporalType.DATE)
+	private Date birthday; // 生日
+	@Column(name = "C_SEX")
+	private Integer sex; // 性别
+	@Column(name = "C_TELEPHONE")
+	private String telephone; // 手机
+	@Column(name = "C_COMPANY")
+	private String company; // 公司
+	@Column(name = "C_DEPARTMENT")
+	private String department; // 部门
+	@Column(name = "C_POSITION")
+	private String position; // 职位
+	@Column(name = "C_ADDRESS")
+	private String address; // 地址
+	@Column(name = "C_MOBILEPHONE")
+	private String mobilePhone; // 座机
+	@Column(name = "C_EMAIL")
+	private String email; // 邮箱
+	@Column(name = "C_Fixed_AREA_ID")
+	private String fixedAreaId; // 定区编码
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getType() {
+		return type;
+	}
+
+	public void setType(Integer type) {
+		this.type = type;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public Integer getSex() {
+		return sex;
+	}
+
+	public void setSex(Integer sex) {
+		this.sex = sex;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMobilePhone() {
+		return mobilePhone;
+	}
+
+	public void setMobilePhone(String mobilePhone) {
+		this.mobilePhone = mobilePhone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFixedAreaId() {
+		return fixedAreaId;
+	}
+
+	public void setFixedAreaId(String fixedAreaId) {
+		this.fixedAreaId = fixedAreaId;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", username=" + username + ", password="
+				+ password + ", type=" + type + ", birthday=" + birthday
+				+ ", sex=" + sex + ", telephone=" + telephone + ", company="
+				+ company + ", department=" + department + ", position="
+				+ position + ", address=" + address + ", mobilePhone="
+				+ mobilePhone + ", email=" + email + ", fixedAreaId="
+				+ fixedAreaId + "]";
+	}
+
+}
